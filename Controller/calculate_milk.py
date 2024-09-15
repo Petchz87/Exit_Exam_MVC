3d396814385e5a350cea3f93c72c5d6298d3b5b7
Petchz87 <zpetza1547@gmail.com>
2024-09-15 12:32:43 +0700

    finish2

--- Controller/calculate_milk.py
+++ Controller/calculate_milk.py
@@ -2,10 +2,14 @@ from Model.check_id import check_animal_data
 from Model.update_udders import update_cow_udder_count, handle_cow_udder_count
 
 def cal_milk(animal_id, udders, age):
+    if udders == 4:
+        milk_production = calculate_milk_production(age)
+    else:
+        milk_production = 0
     updated_udders = handle_cow_udder_count(udders)
     update_cow_udder_count(animal_id, updated_udders)
-    milk_production = calculate_milk_production(age)
     return milk_production
+
 
 def calculate_milk_production(age):
     try:
